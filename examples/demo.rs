98c795e2de5221a4c50c3c268ae73d377ac71882
TakWolf <takwolf@foxmail.com>
2019-12-24 21:36:10 +0800

    demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,4 +1,25 @@
+use tge::error::GameResult;
+use tge::engine::{EngineBuilder, Engine};
+use tge::window::WindowConfig;
+use tge::graphics::GraphicsConfig;
+use tge::timer::TimerConfig;
+
+struct App {}
+
+impl App {
+
+    fn new(engine: &mut Engine) -> GameResult<Self> {
+        Ok(Self {})
+    }
+
+}
 
 fn main() {
-    println!("Hello, world!");
+    let mut engine = EngineBuilder::new()
+        .window_config(WindowConfig::new())
+        .graphics_config(GraphicsConfig::new())
+        .timer_config(TimerConfig::new())
+        .build()
+        .unwrap();
+    let mut app = App::new(&mut engine).unwrap();
 }
