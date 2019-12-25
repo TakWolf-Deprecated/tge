cb7ebaabf8f75c987cadaf468a194e04f5e4d309
TakWolf <takwolf@foxmail.com>
2019-12-26 03:03:56 +0800

    补充角度结构运算

--- src/math/angle.rs
+++ src/math/angle.rs
@@ -1,4 +1,6 @@
 use super::Float;
+use std::ops::{Add, Sub, Mul, Div, AddAssign, SubAssign, MulAssign, DivAssign};
+use std::cmp::Ordering;
 
 #[derive(Debug, Copy, Clone)]
 pub enum Angle<F: Float = f32> {
@@ -31,6 +33,13 @@ impl<F: Float> Angle<F> {
         }
     }
 
+    pub fn set_value(&mut self, new_value: F) {
+        match self {
+            Angle::Radians(value) => *value = new_value,
+            Angle::Degrees(value) => *value = new_value,
+        }
+    }
+
     pub fn radians_value(&self) -> F {
         match self {
             Angle::Radians(value) => *value,
@@ -55,10 +64,114 @@ impl<F: Float> Angle<F> {
 
 }
 
+impl<F: Float> Add for Angle<F> {
+
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        match self {
+            Angle::Radians(value) => Angle::Radians(value + other.radians_value()),
+            Angle::Degrees(value) => Angle::Degrees(value + other.degrees_value()),
+        }
+    }
+
+}
+
+impl<F: Float> Sub for Angle<F> {
+
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        match self {
+            Angle::Radians(value) => Angle::Radians(value - other.radians_value()),
+            Angle::Degrees(value) => Angle::Degrees(value - other.degrees_value()),
+        }
+    }
+
+}
+
+impl<F: Float> AddAssign for Angle<F> {
+
+    fn add_assign(&mut self, other: Self) {
+        match self {
+            Angle::Radians(value) => *value += other.radians_value(),
+            Angle::Degrees(value) => *value += other.degrees_value(),
+        }
+    }
+
+}
+
+impl<F: Float> SubAssign for Angle<F> {
+
+    fn sub_assign(&mut self, other: Self) {
+        match self {
+            Angle::Radians(value) => *value -= other.radians_value(),
+            Angle::Degrees(value) => *value -= other.degrees_value(),
+        }
+    }
+
+}
+
+impl<F: Float> Mul<F> for Angle<F> {
+
+    type Output = Self;
+
+    fn mul(self, rhs: F) -> Self::Output {
+        match self {
+            Angle::Radians(value) => Angle::Radians(value * rhs),
+            Angle::Degrees(value) => Angle::Degrees(value * rhs),
+        }
+    }
+
+}
+
+impl<F: Float> Div<F> for Angle<F> {
+
+    type Output = Self;
+
+    fn div(self, rhs: F) -> Self::Output {
+        match self {
+            Angle::Radians(value) => Angle::Radians(value / rhs),
+            Angle::Degrees(value) => Angle::Degrees(value / rhs),
+        }
+    }
+
+}
+
+impl<F: Float> MulAssign<F> for Angle<F> {
+
+    fn mul_assign(&mut self, rhs: F) {
+        match self {
+            Angle::Radians(value) => *value *= rhs,
+            Angle::Degrees(value) => *value *= rhs,
+        }
+    }
+
+}
+
+impl<F: Float> DivAssign<F> for Angle<F> {
+
+    fn div_assign(&mut self, rhs: F) {
+        match self {
+            Angle::Radians(value) => *value /= rhs,
+            Angle::Degrees(value) => *value /= rhs,
+        }
+    }
+
+}
+
 impl<F: Float> PartialEq for Angle<F> {
 
     fn eq(&self, other: &Self) -> bool {
         self.radians_value() == other.radians_value()
+    }
+
+}
+
+impl<F: Float> PartialOrd for Angle<F> {
+
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.radians_value().partial_cmp(&other.radians_value())
     }
 
 }
