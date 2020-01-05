92bb7a7755975061e81901415facdd280c0d49d7
TakWolf <takwolf@foxmail.com>
2020-01-06 01:42:48 +0800

    事件回调

--- src/game.rs
+++ src/game.rs
@@ -1,10 +1,15 @@
 use crate::error::GameResult;
 use crate::engine::Engine;
+use crate::event::Event;
 
 pub trait Game {
 
     fn update(&mut self, engine: &mut Engine) -> GameResult;
 
     fn render(&mut self, engine: &mut Engine) -> GameResult;
+
+    fn event(&mut self, engine: &mut Engine, event: Event) -> GameResult<bool> {
+        return Ok(false)
+    }
 
 }
