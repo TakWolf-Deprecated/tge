c40c106bb0d6e50db31e0b903d08803422065d87
TakWolf <takwolf@foxmail.com>
2020-02-04 06:02:02 +0800

    初始化 gl 指针

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,18 +1,26 @@
 use crate::error::{GameError, GameResult};
 use winit::window::Window;
 use glutin::{ContextWrapper, PossiblyCurrent};
+use glow::Context;
 use std::rc::Rc;
 
 pub struct Graphics {
     context_wrapper: Rc<ContextWrapper<PossiblyCurrent, Window>>,
+    gl: Rc<Context>,
 }
 
 impl Graphics {
 
     pub(crate) fn new(graphics_config: GraphicsConfig, context_wrapper: Rc<ContextWrapper<PossiblyCurrent, Window>>) -> GameResult<Self> {
+        let gl = Context::from_loader_function(|symbol| context_wrapper.get_proc_address(symbol).cast());
         Ok(Self {
-            context_wrapper
+            context_wrapper,
+            gl: Rc::new(gl),
         })
+    }
+
+    pub(crate) fn gl(&self) -> &Rc<Context> {
+        &self.gl
     }
 
     pub(crate) fn prepare(&mut self) -> GameResult {
