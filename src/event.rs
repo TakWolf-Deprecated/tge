9cba27855aa0c191bdd5afb0096467747d84a0e7
TakWolf <takwolf@foxmail.com>
2020-02-10 03:59:56 +0800

    鼠标模块

--- src/event.rs
+++ src/event.rs
@@ -1,7 +1,26 @@
+use crate::math::{Position, Size};
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+pub enum KeyAction {
+    Down,
+    Up,
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Event {
     AppSuspend,
     AppResume,
     WindowClose,
+    WindowResize(Size<u32>),
+    WindowMove(Position<i32>),
+    WindowFocusChange(bool),
+    // TODO
+}
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+pub(crate) enum KeyState {
+    Down,
+    Hold,
+    Up,
+    Idle,
 }
--- src/mouse.rs
+++ src/mouse.rs
@@ -1,22 +1,73 @@
-use crate::error::GameResult;
+mod cursor;
+mod button;
 
-pub struct Mouse {}
+pub use cursor::CursorIcon;
+pub use button::MouseButton;
+
+use crate::error::GameResult;
+use crate::math::Position;
+use crate::event::KeyState;
+use winit::window::Window;
+use glutin::{ContextWrapper, PossiblyCurrent};
+use std::rc::Rc;
+use std::collections::HashMap;
+
+pub struct Mouse {
+    context_wrapper: Rc<ContextWrapper<PossiblyCurrent, Window>>,
+    cursor_icon: CursorIcon,
+    cursor_visible: bool,
+    position: Position,
+    inside_window: bool,
+    button_states: HashMap<MouseButton, KeyState>,
+    wheel_delta: f32,
+}
 
 impl Mouse {
 
-    pub(crate) fn new(mouse_config: MouseConfig) -> GameResult<Self> {
-        Ok(Self {})
+    pub(crate) fn new(mouse_config: MouseConfig, context_wrapper: Rc<ContextWrapper<PossiblyCurrent, Window>>) -> GameResult<Self> {
+        let window = context_wrapper.window();
+        window.set_cursor_icon(mouse_config.cursor_icon.into());
+        window.set_cursor_visible(mouse_config.cursor_visible);
+        Ok(Self {
+            context_wrapper,
+            cursor_icon: mouse_config.cursor_icon,
+            cursor_visible: mouse_config.cursor_visible,
+            position: Position::zero(),
+            inside_window: false,
+            button_states: HashMap::new(),
+            wheel_delta: 0.0,
+        })
+    }
+
+    fn window(&self) -> &Window {
+        self.context_wrapper.window()
     }
 
 }
 
 #[derive(Debug, Clone)]
-pub struct MouseConfig {}
+pub struct MouseConfig {
+    cursor_icon: CursorIcon,
+    cursor_visible: bool,
+}
 
 impl MouseConfig {
 
     pub fn new() -> Self {
-        Self {}
+        Self {
+            cursor_icon: CursorIcon::default(),
+            cursor_visible: true,
+        }
+    }
+
+    pub fn cursor_icon(mut self, cursor_icon: CursorIcon) -> Self {
+        self.cursor_icon = cursor_icon;
+        self
+    }
+
+    pub fn cursor_visible(mut self, cursor_visible: bool) -> Self {
+        self.cursor_visible = cursor_visible;
+        self
     }
 
 }
