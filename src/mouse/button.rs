ab06697a735482d9abe30d8f93ceed1602ed5b66
TakWolf <takwolf@foxmail.com>
2020-02-10 03:41:36 +0800

    button枚举

---
+++ src/mouse/button.rs
@@ -0,0 +1,21 @@
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+pub enum MouseButton {
+    Left,
+    Right,
+    Middle,
+    Other(u8),
+}
+
+impl From<winit::event::MouseButton> for MouseButton {
+
+    fn from(button: winit::event::MouseButton) -> Self {
+        match button {
+            winit::event::MouseButton::Left => MouseButton::Left,
+            winit::event::MouseButton::Right => MouseButton::Right,
+            winit::event::MouseButton::Middle => MouseButton::Middle,
+            winit::event::MouseButton::Other(button_id) => MouseButton::Other(button_id.into()),
+        }
+    }
+
+}
