7d9b84ab94f2fd4cd9a0aad994b127ccfa8eb1ec
TakWolf <takwolf@foxmail.com>
2020-02-23 19:30:37 +0800

    新的 error api

--- src/error.rs
+++ src/error.rs
@@ -3,27 +3,40 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum GameError {
-    IoError(String),
-    InitError(String),
-    StateError(String),
-    RuntimeError(String),
-    NotSupportedError(String),
+    IoError(Box<dyn Error>),
+    InitError(Box<dyn Error>),
+    StateError(Box<dyn Error>),
+    RuntimeError(Box<dyn Error>),
+    NotSupportedError(Box<dyn Error>),
+}
+
+impl Error for GameError {
+
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        let source = match self {
+            GameError::IoError(source) => source,
+            GameError::InitError(source) => source,
+            GameError::StateError(source) => source,
+            GameError::RuntimeError(source) => source,
+            GameError::NotSupportedError(source) => source,
+        };
+        Some(source.as_ref())
+    }
+
 }
 
 impl fmt::Display for GameError {
 
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            GameError::IoError(message) => write!(fmt, "GameError::IoError: {}", message),
-            GameError::InitError(message) => write!(fmt, "GameError::InitError: {}", message),
-            GameError::StateError(message) => write!(fmt, "GameError::StateError: {}", message),
-            GameError::RuntimeError(message) => write!(fmt, "GameError::RuntimeError: {}", message),
-            GameError::NotSupportedError(message) => write!(fmt, "GameError::NotSupportedError: {}", message),
+            GameError::IoError(source) => write!(fmt, "GameError::IoError: {}", source),
+            GameError::InitError(source) => write!(fmt, "GameError::InitError: {}", source),
+            GameError::StateError(source) => write!(fmt, "GameError::StateError: {}", source),
+            GameError::RuntimeError(source) => write!(fmt, "GameError::RuntimeError: {}", source),
+            GameError::NotSupportedError(source) => write!(fmt, "GameError::NotSupportedError: {}", source),
         }
     }
 
 }
-
-impl Error for GameError {}
 
 pub type GameResult<T = ()> = Result<T, GameError>;
