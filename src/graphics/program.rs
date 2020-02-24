5694d7bd2e615f60b6f1edf7e46406918c2964c0
TakWolf <takwolf@foxmail.com>
2020-02-25 00:19:06 +0800

    program 包装

---
+++ src/graphics/program.rs
@@ -0,0 +1,42 @@
+use super::opengl;
+use crate::error::{GameError, GameResult};
+use crate::engine::Engine;
+use glow::Context;
+use std::rc::Rc;
+
+const DEFAULT_VERTEX_SHADER_SOURCE: &str = include_str!("shaders/default.vert");
+const DEFAULT_FRAGMENT_SHADER_SOURCE: &str = include_str!("shaders/default.frag");
+
+pub struct Program {
+    program: Rc<opengl::Program>,
+}
+
+impl Program {
+
+    pub fn new(
+        engine: &mut Engine,
+        vertex_shader_source: &str,
+        fragment_shader_source: &str,
+    ) -> GameResult<Self> {
+        let program = opengl::Program::new(
+            engine.graphics().gl().clone(),
+            vertex_shader_source,
+            fragment_shader_source,
+        ).map_err(|error| GameError::InitError(error.into()))?;
+        Ok(Self { program: Rc::new(program) })
+    }
+
+    pub(crate) fn default(gl: Rc<Context>) -> GameResult<Rc<opengl::Program>> {
+        let program = super::opengl::Program::new(
+            gl,
+            DEFAULT_VERTEX_SHADER_SOURCE,
+            DEFAULT_FRAGMENT_SHADER_SOURCE,
+        ).map_err(|error| GameError::InitError(error.into()))?;
+        Ok(Rc::new(program))
+    }
+
+    pub(crate) fn program(&self) -> &Rc<opengl::Program> {
+        &self.program
+    }
+
+}
