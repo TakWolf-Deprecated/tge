8cdc63aa3bd6aa38c5046f5610090a9fe8c71770
TakWolf <takwolf@foxmail.com>
2020-03-06 22:47:09 +0800

    调整动画例子

--- examples/animation.rs
+++ examples/animation.rs
@@ -1,52 +1,36 @@
 use tge::error::GameResult;
-use tge::math::Region;
+use tge::math::{Size, Region};
 use tge::engine::{Engine, EngineBuilder};
 use tge::window::WindowConfig;
 use tge::graphics::*;
 use tge::game::Game;
+use std::time::Duration;
 
 const TITLE: &str = "Animation";
 
 struct App {
-    character: Texture,
-    frames: Vec<Region>,
-    current_frame: usize,
-    current_frame_time: f32,
+    texture: Texture,
     animation_fps: f32,
+    total_frame: usize,
+    frame_size: Size,
+    current_frame: usize,
+    current_frame_time: Duration,
 }
 
 impl App {
 
     fn new(engine: &mut Engine) -> GameResult<Self> {
-        let character = Texture::load(engine, "assets/pixel-character/pixel-character.png")?;
-        let mut sheet = Vec::new();
-        let frame_size = 384.0 / 8.0;
-        for j in 0..8 {
-            for i in 0..8 {
-                sheet.push(Region::new(
-                    i as f32 * frame_size,
-                    j as f32 * frame_size,
-                    frame_size,
-                    frame_size,
-                ));
-            }
-        }
-        let frames = vec![
-            *&sheet[24],
-            *&sheet[25],
-            *&sheet[26],
-            *&sheet[27],
-            *&sheet[28],
-            *&sheet[29],
-            *&sheet[30],
-            *&sheet[31],
-        ];
+        let texture = Texture::load(engine, "assets/coin.png")?;
+        let texture_size = texture.size();
+        let total_frame = 8;
+        let frame_size = Size::new(texture_size.width as f32 / total_frame as f32, texture_size.height as f32);
         Ok(Self {
-            character,
-            frames,
+            texture,
+            animation_fps: 10.0,
+            total_frame,
+            frame_size,
             current_frame: 0,
-            current_frame_time: 0.0,
-            animation_fps: 16.0,
+            current_frame_time: Duration::new(0, 0),
         })
     }
 
@@ -58,12 +42,11 @@ impl Game for App {
         let title = format!("{} - FPS: {}", TITLE, engine.timer().real_time_fps().round());
         engine.window().set_title(title);
 
-        let delta_time = engine.timer().delta_time().as_secs_f32();
-        self.current_frame_time += delta_time;
-        if self.current_frame_time >= 1.0 / self.animation_fps {
-            self.current_frame_time = 0.0;
+        self.current_frame_time += engine.timer().delta_time();
+        if self.current_frame_time.as_secs_f32() >= 1.0 / self.animation_fps {
+            self.current_frame_time = Duration::new(0, 0);
             self.current_frame += 1;
-            if self.current_frame >= self.frames.len() {
+            if self.current_frame >= self.total_frame {
                 self.current_frame = 0;
             }
         }
@@ -74,12 +57,23 @@ impl Game for App {
     fn render(&mut self, engine: &mut Engine) -> GameResult {
         engine.graphics().clear(Color::BLACK);
 
-        engine.graphics().draw_sprite(
-            Some(&self.character),
-            SpriteDrawParams::default()
-                .region(*&self.frames[self.current_frame])
-                .scale((8.0, 8.0)),
+        let region = Region::new(
+            self.frame_size.width * self.current_frame as f32,
+            0.0,
+            self.frame_size.width,
+            self.frame_size.height,
         );
+        for j in 0..8 {
+            for i in 0..8 {
+                engine.graphics().draw_sprite(
+                    Some(&self.texture),
+                    SpriteDrawParams::default()
+                        .position((i as f32 * self.frame_size.width * 2.0, j as f32 * self.frame_size.height * 2.0))
+                        .region(region)
+                        .scale((2.0, 2.0)),
+                );
+            }
+        }
 
         Ok(())
     }
@@ -90,7 +84,7 @@ fn main() -> GameResult {
     EngineBuilder::new()
         .window_config(WindowConfig::new()
             .title(TITLE)
-            .inner_size((384, 384))
+            .inner_size((16 * 16, 16 * 16))
             .resizable(false))
         .graphics_config(GraphicsConfig::new()
             .default_filter(Filter::new(
