176b731df1e90b405ce9e83e0c137706647ae659
TakWolf <takwolf@foxmail.com>
2020-08-07 19:13:20 +0800

    调整 dvd demo

--- examples/dvd.rs
+++ examples/dvd.rs
@@ -4,6 +4,8 @@ use tge::engine::{Engine, EngineBuilder};
 use tge::window::WindowConfig;
 use tge::graphics::*;
 use tge::game::Game;
+use rand::Rng;
+use rand::rngs::ThreadRng;
 
 const TITLE: &str = "DVD";
 
@@ -12,6 +14,8 @@ struct App {
     size: Size,
     position: Position,
     speed: Vector,
+    color: Color,
+    rand: ThreadRng,
 }
 
 impl App {
@@ -25,12 +29,23 @@ impl App {
         let graphics_size = engine.graphics().size();
         let position = Position::new(graphics_size.width / 2.0, graphics_size.height / 2.0);
         let speed = Vector::new(1.0, 1.0);
+        let color = Color::new(0.5, 0.5, 1.0, 1.0);
+        let rand = rand::thread_rng();
         Ok(Self {
             dvd_logo,
             size,
             position,
             speed,
+            color,
+            rand,
         })
+    }
+
+    fn set_logo_random_color(&mut self) {
+        let red = self.rand.gen::<f32>() * 0.6 + 0.4;
+        let green = self.rand.gen::<f32>() * 0.6 + 0.4;
+        let blue = self.rand.gen::<f32>() * 0.6 + 0.4;
+        self.color = Color::new(red, green, blue, 1.0);
     }
 
 }
@@ -46,16 +61,20 @@ impl Game for App {
         if self.position.x < self.size.width / 2.0 {
             self.position.x = self.size.width / 2.0;
             self.speed.x *= -1.0;
+            self.set_logo_random_color();
         } else if self.position.x > graphics_size.width - self.size.width / 2.0 {
             self.position.x = graphics_size.width - self.size.width / 2.0;
             self.speed.x *= -1.0;
+            self.set_logo_random_color();
         }
         if self.position.y < self.size.height / 2.0 {
             self.position.y = self.size.height / 2.0;
             self.speed.y *= -1.0;
+            self.set_logo_random_color();
         } else if self.position.y > graphics_size.height - self.size.height / 2.0 {
             self.position.y = graphics_size.height - self.size.height / 2.0;
             self.speed.y *= -1.0;
+            self.set_logo_random_color();
         }
 
         Ok(())
@@ -67,7 +86,7 @@ impl Game for App {
         engine.graphics().draw_sprite(
             &self.dvd_logo,
             SpriteDrawParams::default()
-                .color((0.5, 0.5, 1.0, 1.0)),
+                .color(self.color),
             TransformParams::default()
                 .position(self.position)
                 .origin((self.size.width / 2.0, self.size.height / 2.0)),
