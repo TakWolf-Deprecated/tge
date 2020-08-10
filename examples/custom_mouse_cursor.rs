75331e6c1a951ca1d9aef5432478ecd9e3fc34b2
TakWolf <takwolf@foxmail.com>
2020-08-10 15:01:21 +0800

    自定义鼠标光标示例

---
+++ examples/custom_mouse_cursor.rs
@@ -0,0 +1,59 @@
+use tge::error::GameResult;
+use tge::engine::{Engine, EngineBuilder};
+use tge::window::WindowConfig;
+use tge::graphics::*;
+use tge::mouse::MouseConfig;
+use tge::game::Game;
+
+const TITLE: &str = "Custom Mouse Cursor";
+
+struct App {
+    cursor: Texture,
+}
+
+impl App {
+
+    fn new(engine: &mut Engine) -> GameResult<Self> {
+        let cursor = Texture::load(engine, "assets/cursor.png")?;
+        Ok(Self {
+            cursor,
+        })
+    }
+
+}
+
+impl Game for App {
+
+    fn update(&mut self, engine: &mut Engine) -> GameResult {
+        let title = format!("{} - FPS: {}", TITLE, engine.timer().real_time_fps().round());
+        engine.window().set_title(title);
+        Ok(())
+    }
+
+    fn render(&mut self, engine: &mut Engine) -> GameResult {
+        engine.graphics().clear(Color::WHITE);
+
+        if let Some(position) = engine.mouse().position() {
+            engine.graphics().draw_sprite(
+                &self.cursor,
+                SpriteDrawParams::default(),
+                TransformParams::default()
+                    .position(position),
+            );
+        }
+
+        Ok(())
+    }
+
+}
+
+fn main() -> GameResult {
+    EngineBuilder::new()
+        .window_config(WindowConfig::new()
+            .title(TITLE)
+            .inner_size((800.0, 600.0)))
+        .mouse_config(MouseConfig::new()
+            .cursor_visible(false))
+        .build()?
+        .run_with(App::new)
+}
